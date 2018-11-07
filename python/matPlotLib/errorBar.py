094fe867d23a0fb1eb0510ba952392d5dbfeeda3
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-11-07 22:56:40 +0700

    Create errorBar.py

---
+++ python/matPlotLib/errorBar.py
@@ -0,0 +1,15 @@
+fig = plt.figure(0)
+x = np.arange(10.0)
+y = np.sin(np.arange(10.0) / 20.0 * np.pi)
+
+plt.errorbar(x, y, yerr=0.1)
+
+y = np.sin(np.arange(10.0) / 20.0 * np.pi) + 1
+plt.errorbar(x, y, yerr=0.1, uplims=True)
+
+y = np.sin(np.arange(10.0) / 20.0 * np.pi) + 2
+upperlimits = np.array([1, 0] * 5)
+lowerlimits = np.array([0, 1] * 5)
+plt.errorbar(x, y, yerr=0.1, uplims=upperlimits, lolims=lowerlimits)
+
+plt.xlim(-1, 10)
