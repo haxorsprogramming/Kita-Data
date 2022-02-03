2f91f6b63d2d0fe6c77e196cc7a5e25810d697cb
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2022-02-03 15:38:33 +0700

    Create kmeans.py

---
+++ classification/kmeans.py
@@ -0,0 +1,38 @@
+#importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing dataset
+dataset=pd.read_csv("Mall_Customers.csv")
+x=dataset.iloc[:,[3,4]].values
+
+#Using the elbow method to find the number of clusters
+from sklearn.cluster import KMeans
+wcss=[]
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
+    kmeans.fit(x)
+wcss.append(kmeans.inertia_)
+plt.plot(range(1,11),wcss)
+plt.title('the Elbow method')
+plt.xlabel('number of clusters')
+plt.ylabel('wcss')
+plt.show()
+
+#Applying K-Means to the mall dataset
+kmeans=KMeans(n_clusters=5,init='k-means++',max_iter=300,n_init=10,random_state=0)
+y_kmeans=kmeans.fit_predict(x)
+
+#Visualising the Clusters
+plt.scatter(x[y_kmeans==0,0],x[y_kmeans==0,1],s=100,c='red',label='Carefull')
+plt.scatter(x[y_kmeans==1,0],x[y_kmeans==1,1],s=100,c='blue',label='Standard')
+plt.scatter(x[y_kmeans==2,0],x[y_kmeans==2,1],s=100,c='green',label='Target')
+plt.scatter(x[y_kmeans==3,0],x[y_kmeans==3,1],s=100,c='cyan',label='Careless')
+plt.scatter(x[y_kmeans==4,0],x[y_kmeans==4,1],s=100,c='magenta',label='Sensible')
+plt.scatter(kmeans.cluster_centers_[0:,0],kmeans.cluster_centers_[:,1],s=300,c='Yellow',label='Centroids')
+plt.title('Clusters of Clients')
+plt.xlabel('Annual Income(K$)')
+plt.ylabel('Spending Score(1=100)')
+plt.legend()
+plt.show()
